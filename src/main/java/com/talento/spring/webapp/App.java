49c4701a102678f56c89c44ad31dae0a1cdcc7ca
Emmanuel389 <emmanuel.nietomu@gmail.com>
2021-04-04 08:12:52 -0400

    Spring Framework 5 , creado desde Maven, Pruebas de Seccion 8: Spring

---
+++ src/main/java/com/talento/spring/webapp/App.java
@@ -0,0 +1,13 @@
+package com.talento.spring.webapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
